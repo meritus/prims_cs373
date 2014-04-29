99a18fcaa7ece1c92ef420bd6c657010755a9756
eliu7 <eliu7@binghamton.edu>
2014-04-29 19:14:45 -0400

    initial commit

---
+++ prims/prims/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  prims
+//
+//  Created by Edison Liu on 4/29/14.
+//  Copyright (c) 2014 ___CS373___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
