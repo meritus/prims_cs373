cf2aacbc7430f2840979b58c3ef1b72da982799d
eliu7 <eliu7@binghamton.edu>
2014-04-30 18:47:42 -0400

    works

--- prims/prims/main.cpp
+++ prims/prims/main.cpp
@@ -11,58 +11,126 @@
 #include <queue>
 #include <algorithm>
 #include <limits.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-struct node{
+struct Node{
     int name;
     int weight;
 };
 
 //used to sort the heap
-bool compare(const node *lhs, const node *rhs) {
+bool compare(const Node *lhs, const Node *rhs) {
     return lhs->weight>rhs->weight;
 }
 
 
 int *array_maker(int a, int b, int c, int d);
 int *prims(int num_nodes, int start_node, int **adj);
+int** createUserGraph();
+void printGraph(int** graph);
 
 int main(int argc, const char * argv[])
 {
-    node one;
-    node two;
-    node three;
-    one.name=1;
-    one.weight=10;
-    two.name=2;
-    two.weight=12;
-    three.name=3;
-    three.weight=6;
-    
-    vector<node> pq;
-    pq.push_back(two);
-    pq.push_back(one);
-    pq.push_back(three);
-    //make_heap(pq.begin(), pq.end(), &compare);
-    
-    //cout<<pq.front().name<<endl;
-    
-    int **adj_mat = new int*[4];
-    adj_mat[0]=array_maker(0, 7, -1, 15);
-    adj_mat[1]=array_maker(7,0,9,-1);
-    adj_mat[2]=array_maker(-1, 9, 0, -1);
-    adj_mat[3]=array_maker(15, -1, -1, 0);
-    
-    /*
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++)
-            cout<<adj_mat[i][j]<<" ";
+    int** graph;
+    int node;
+    //if user entered a file than use that file
+	if(argv[1])
+	{
+		
+		string inputFile;
+		ifstream infile(argv[1]);
+		//check if file is there
+		if(!infile)
+		{
+			cout << "Could not open input file" << endl;
+			exit(0);
+		}
+		//opens input file and stores it in varible input
+		if(infile.is_open())
+		{
+			infile >> inputFile;
+		}
+		else
+		{
+			exit(0);
+		}
+		//convert first line to int because that line determines number of nodes
+		node = atoi( inputFile.c_str());
+		//creates 2 dimensionanl array with row and cols eqaul to node
+		graph = new int*[node];
+        for(int i = 0; i < node; i++)
+		{
+            graph[i] = new int[node];
+			for(int j = 0; j < node; j++)
+            {
+                graph[i][j] = (i+j);
+            }
+        }
+		int temp = -1;
+		// inserst into nodes and column form mteh file
+		for(int i = 0; i<node; i++)
+        {
+			//set inputfile to next line
+			infile >> inputFile;
+			int temp2 = 0;
+			for(int j = 0; j<node; j++)
+            {
+                //checks current line until - whic means new number thatn everything before that is value sotred
+                string tempstr;
+                int x = temp2;
+                while( x < inputFile.length())
+                {
+                    if(inputFile[x] != ',')
+                    {
+                        tempstr += inputFile[x];
+                    }
+                    else
+                    {
+                        x = inputFile.length()+1;
+                    }
+                    x++;
+                    temp2++;
+                }
+                //convert to a int
+                temp = atoi( tempstr.c_str());
+                //if the value is zero set it to 1 that means it is infinty
+                //else set it to the value indicated by user
+                //if the the value is zero than we dont have direct acces to that node form this spot whci means we dont know how far away its so its infinty
+                //-1=infinty
+                
+                graph[i][j] = temp;
+                //temp = -1;
+            }
+        }
+		//close the file
+		infile.close();
+	}
+	else
+	{
+		//printGraph(createUserGraph());
+	}
+
+    prims(node,0,graph);
+}
+
+void printGraph(int** graph)
+{
+	for (int x=0; x<7; x++)
+	{
+        
+        for (int y=0; y<7; y++)
+        {
+            
+	        cout<<graph[x][y] <<" ";
+        }
         cout<<endl;
-    }
-    */
-    
-    prims(4,0,adj_mat);
+	}
+	
 }
 
 int *array_maker(int a, int b, int c, int d){
@@ -76,9 +144,9 @@ int *array_maker(int a, int b, int c, int d){
 
 
 int *prims(int num_nodes, int start_node, int **adj){
-    vector<node*> pq;
+    vector<Node*> pq;
     int answer[num_nodes];
-    node nodes_arr[num_nodes];
+    Node nodes_arr[num_nodes];
     bool in_pq[num_nodes];
     //initalize the nodes and put into priorityqueue
     for(int i=0;i<num_nodes;i++){
@@ -92,7 +160,7 @@ int *prims(int num_nodes, int start_node, int **adj){
         in_pq[i]=true;
     }
     while (pq.size()>0){
-        node *u=pq.front(); //get min items
+        Node *u=pq.front(); //get min items
         //rearrage the elementes to heap is shortened by 1, min item moves to the end of heap
         pop_heap(pq.begin(), pq.end(), &compare);
         pq.pop_back(); //pop it off
@@ -100,7 +168,7 @@ int *prims(int num_nodes, int start_node, int **adj){
         cout<<"current popped: "<<"index: "<<u->name<<endl;
         for(int i=0;i<num_nodes;i++){ //so that it loops through all the possible adj nodes
             if(adj[u->name][i]==-1) continue; //means that it's not adj
-            node v;
+            Node v;
             v.name=i;
             v.weight=adj[u->name][i];
             if(in_pq[i] && adj[u->name][i]<nodes_arr[i].weight){ //if in queue weight less than key
@@ -115,4 +183,52 @@ int *prims(int num_nodes, int start_node, int **adj){
     }
     return 0;
 }
+
+//if return -1 it failed 0 passed
+int** createUserGraph()
+{
+	int node = -1;
+	//ask for the amoutn of nodes from user
+	//has to be postive non zero number
+	while(node <= 0)
+	{
+		cout << "Enter (positve non-zero) amount of nodes for the graph: ";
+		cin>>node;
+	}
+	//creates 2 dimensional array node by node
+	int** graph = new int*[node];
+    for(int i = 0; i < node; i++)
+	{
+        graph[i] = new int[node];
+        for(int j = 0; j < node; j++)
+        {
+            graph[i][j] = (i+j);
+        }
+    }
+	int temp = -1;
+	cout<< "Enter the Adjency matrix values"<<endl;
+	for(int i = 0; i<node; i++)
+    {
+		for(int j = 0; j<node; j++)
+        {
+            //get value from user
+            //test so user will input a positve vaule if negative or does not input a value than asks again till correct
+            
+            cout<<"Enter a postive value for row "<< (i+1) << " col " << (j+1) << endl;
+            cin >>temp;
+            
+            //if the value is zero set it to 1 that means it is infinty
+            //else set it to the value indicated by user
+            //if the the value is zero than we dont have direct acces to that node form this spot whci means we dont know how far away its so its infinty
+            //-1=infinty
+    
+            graph[i][j] = temp;
+            
+			
+        }
+    }	
+	//return the graph
+	return graph;
+}
+
 
