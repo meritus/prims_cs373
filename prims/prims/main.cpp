dcfd90bf37b3c56a8b2cf386b3f10c6fcecd18e7
eliu7 <eliu7@binghamton.edu>
2014-04-30 19:59:41 -0400

    working master

--- prims/prims/main.cpp
+++ prims/prims/main.cpp
@@ -34,15 +34,7 @@ int *prims(int num_nodes, int start_node, int **adj);
 int** createUserGraph();
 void printGraph(int** graph);
 
-using namespace std;
-
-
-//create a grpah of nodes with distance between adjecnt nodes
-//compute the prim algorithm on the graph and print out result
-
-
-
-int main(int argc, char* argv[])
+int main(int argc, const char * argv[])
 {
     int** graph;
     int node;
@@ -122,96 +114,7 @@ int main(int argc, char* argv[])
 	{
 		//printGraph(createUserGraph());
 	}
-
-<<<<<<< HEAD
-//if users typed a a file name than use it else use ask user for input
-	cout<<"hello world"<<endl;
-	//if user entered a file than use that file
-	if(argv[1])
-	{
-		
-		string inputFile; 	
-		ifstream infile(argv[1]);
-		//check if file is there
-		if(!infile) 
-		{
-			cout << "Could not open input file" << endl;
-			exit(0);
-		}
-		//opens input file and stores it in varible input
-		if(infile.is_open()) 
-		{
-			infile >> inputFile;
-		}
-		else
-		{
-			exit(0);
-		}
-		//convert first line to int because that line determines number of nodes
-		int node = atoi( inputFile.c_str());
-		//creates 2 dimensionanl array with row and cols eqaul to node
-		int** graph = new int*[node];
-	    	for(int i = 0; i < node; i++) 
-		{
-	       		graph[i] = new int[node]; 
-			for(int j = 0; j < node; j++)
-				{ 
-					graph[i][j] = (i+j); 
-				}   
-	  	  }
-		int temp = -1;
-		// inserst into nodes and column form mteh file
-		for(int i = 0; i<node; i++)
-			{
-			//set inputfile to next line
-			infile >> inputFile;
-			int temp2 = 0;
-			for(int j = 0; j<node; j++)
-				{
-					//checks current line until - whic means new number thatn everything before that is value sotred				
-					string tempstr;
-					int x = temp2;
-					while( x < inputFile.length())
-					{
-						if(inputFile[x] != '-')
-						{					
-							tempstr += inputFile[x];
-						}
-						else
-						{
-							x = inputFile.length()+1;
-						}
-						x++;
-						temp2++;
-					}
-					//convert to a int
-					temp = atoi( tempstr.c_str());
-					//if the value is zero set it to 1 that means it is infinty
-					//else set it to the value indicated by user
-					//if the the value is zero than we dont have direct acces to that node form this spot whci means we dont know how far away its so its infinty
-					//-1=infinty
-					if(temp == 0)
-					{
-						graph[i][j] = INT_MAX;
-					}
-					else
-					{
-						graph[i][j] = temp;
-					}
-					temp = -1;
-				}
-			}
-		//close the file
-		infile.close();
-		printGraph(graph);
-	}
-	else
-	{
-		printGraph(createUserGraph());
-	}
-	
-	return 0;
-=======
+    
     prims(node,0,graph);
 }
 
@@ -228,24 +131,8 @@ void printGraph(int** graph)
         cout<<endl;
 	}
 	
->>>>>>> prims_function
 }
-void printGraph(int** graph)
-{
-	for (int x=0; x<7; x++)
-	{ 
 
-   	 for (int y=0; y<7; y++)
-	 {
-
-<<<<<<< HEAD
-	        cout<<graph[x][y] <<" ";
-	 }
-	 cout<<endl;
-	}
-	
-}
-=======
 int *array_maker(int a, int b, int c, int d){
     int *ret = new int[4];
     ret[0]=a;
@@ -297,7 +184,6 @@ int *prims(int num_nodes, int start_node, int **adj){
     return 0;
 }
 
->>>>>>> prims_function
 //if return -1 it failed 0 passed
 int** createUserGraph()
 {
@@ -311,46 +197,6 @@ int** createUserGraph()
 	}
 	//creates 2 dimensional array node by node
 	int** graph = new int*[node];
-<<<<<<< HEAD
-    	for(int i = 0; i < node; i++) 
-	{
-       		graph[i] = new int[node]; 
-        	for(int j = 0; j < node; j++)
-			{ 
-				graph[i][j] = (i+j); 
-			}   
-  	  }
-	int temp = -1;
-	cout<< "Enter the Adjency matrix values"<<endl;
-	for(int i = 0; i<node; i++)
-		{
-		for(int j = 0; j<node; j++)
-			{
-				//get value from user
-				//test so user will input a positve vaule if negative or does not input a value than asks again till correct
-	
-					cout<<"Enter a postive value for row "<< (i+1) << " col " << (j+1) << endl;				
-					cin >>temp;
-				
-				//if the value is zero set it to 1 that means it is infinty
-				//else set it to the value indicated by user
-				//if the the value is zero than we dont have direct acces to that node form this spot whci means we dont know how far away its so its infinty
-				//-1=infinty
-				if(temp == 0)
-				{
-					graph[i][j] = INT_MAX;
-				}
-				else
-				{
-					graph[i][j] = temp;
-				}
-			
-			}
-		}	
-	//return the graph
-	return graph;
-}
-=======
     for(int i = 0; i < node; i++)
 	{
         graph[i] = new int[node];
@@ -375,15 +221,14 @@ int** createUserGraph()
             //else set it to the value indicated by user
             //if the the value is zero than we dont have direct acces to that node form this spot whci means we dont know how far away its so its infinty
             //-1=infinty
-    
+            
             graph[i][j] = temp;
             
 			
         }
-    }	
+    }
 	//return the graph
 	return graph;
 }
 
 
->>>>>>> prims_function
